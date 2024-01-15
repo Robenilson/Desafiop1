5abf446530941d55b1c32212077cd0f2386dc9e4
Robenilson <robensilva66@gmail.com>
2024-01-15 09:29:37 -0300

    create User entity

---
+++ src/main/java/com/roben/demo/domain/user/User.java
@@ -0,0 +1,33 @@
+package com.roben.demo.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private  String fristName;
+    private  String lastName;
+    @Column(unique = true)
+    private  String document;
+    @Column(unique = true)
+    private String email;
+    private  String password;
+    private BigDecimal banlance;
+    @Enumerated(EnumType.STRING)
+    private  UserType userType;
+}
---
+++ src/main/java/com/roben/demo/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.roben.demo.domain.user;
+
+public enum UserType {
+    COMMOW,
+    MERCHANT
+}
